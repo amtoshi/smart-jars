3cac5a094e64b36779af137d8af069c205e74a02
Ashutosh Mane <itisashutoshmane@gmail.com>
2022-10-23 05:16:43 +0300

    commit

--- Smart Jars/Smart Jars/ViewController.swift
+++ Smart Jars/Smart Jars/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var balanceCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,3 +19,14 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController:UICollectionViewDataSource, UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    
+}
